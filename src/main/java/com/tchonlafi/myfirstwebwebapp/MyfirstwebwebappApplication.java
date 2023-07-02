cfc8aa184019ff6b83ea67361ddc2a48fb3b7cde
Levi <Levi@TheLeviathans>
2023-07-02 23:14:28 +0200

    My first commit

---
+++ src/main/java/com/tchonlafi/myfirstwebwebapp/MyfirstwebwebappApplication.java
@@ -0,0 +1,13 @@
+package com.tchonlafi.myfirstwebwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstwebwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstwebwebappApplication.class, args);
+	}
+
+}
